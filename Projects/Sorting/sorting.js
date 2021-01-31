f6de34697b37ca6d0f56218df535a4ae08be9ca6
AlexCoffey24 <AlexCoffey24@gmail.com>
2021-01-31 18:00:27 -0500

    sorting added

--- Projects/Sorting/sorting.js
+++ Projects/Sorting/sorting.js
@@ -1,3 +1,18 @@
+//  Nav toggle
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
+
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
+});
+
+// Main
 document.getElementById("new").addEventListener("click", new_icicles);
 
 document.getElementById("solve").addEventListener("click", sort);
